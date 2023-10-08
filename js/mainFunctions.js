7242695f2b2f260e61aeec422cc9e468b5efe4cd
altinmullaidrizi <altin.mullaidrizi1@gmail.com>
2023-10-08 13:01:42 +0200

    listing page wip

---
+++ js/mainFunctions.js
@@ -0,0 +1,9 @@
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+    toggler[i].addEventListener("click", function() {
+        this.parentElement.querySelector(".nested").classList.toggle("active-menu");
+        this.classList.toggle("caret-down");
+    });
+}
